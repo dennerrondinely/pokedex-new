6230dd9fc1b1406b30efa137c6adc2ba3d5425f6
Denner Rondinely <denner.rondinely@gmail.com>
2024-05-13 11:22:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis in
+      odit recusandae provident quos nesciunt eius libero dicta saepe.
+      Repudiandae nostrum iure voluptate libero blanditiis. Iusto hic iure quis
+      laudantium.
+    </div>
+  );
+}
+
+export default App;
